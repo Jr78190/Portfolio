5d83afcd40c6f01d00c61b9287cf2b46dcb3b65c
Jr78190 <jeanrayan.sb@gmail.com>
2024-12-12 22:19:14 +0100

    vf

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -11,11 +11,11 @@ const About = () => {
             <div className="about-text">
                 <p> À Propos de moi </p>
                 <h1>Je Suis</h1>
-                <h2>Développeur web React</h2>
+                <h2>Développeur Web </h2>
                 <h3>
                 Bonjour je m'appelle Jean-Rayan, Développeur web junior, reconverti depuis 2022, j'ai débuté à 
-                <span> l'ETNA</span> en bac+2 Avant une interruption pour manque de stage. En 2023, j'ai poursuivi 
-                ma formation d'intégrateur web chez<span> OpenClassrooms </span>  
+                <span> l'ETNA</span> en Bac+2 où j'ai acquis des bases solides en développement. En 2023, j'ai poursuivi 
+                ma formation d'intégrateur web chez<span> OpenClassrooms </span> ce qui m'a permis de renforcer mes compétences,de m’auto-former sur de nouvelles technologies et de m’adapter rapidement à différents outils.
                 À partir de Mars 2025, je reprendrai mes études en alternance en <span>Bachelor 3 "Concepteur de projets SI"</span> à l'ETNA.
                 </h3>
             </div>
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -13,15 +13,14 @@ const Navbar = () => {
             <ul>
                 <li className="item-all"><a href="#competences">Compétences</a></li>
                 <li className="item-all"><a href="#projets">Projets</a></li>
-                <li className='item-all'><a href="#contact">Contact</a></li>
-                <li className="item-contact"><a href="CVJR.pdf" target="_blank" rel="noopener noreferrer">CV</a></li>
+                <li className="item-all"><a href="#contact">Contact</a></li>
+                <li className="item-cv">
+                    <a href="/Cv.pdf" download="CV Bachelor 3">CV</a></li>
             </ul>
         </div>
-        
     </nav>
    </header> 
-
   );
-}
+};
 
 export default Navbar;
