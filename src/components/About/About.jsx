2539bd25fb8b0e7fc66c3efbfbb5811767b7bcc2
Jr78190 <jeanrayan.sb@gmail.com>
2024-12-14 13:15:42 +0100

    vf

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -15,8 +15,8 @@ const About = () => {
                 <h3>
                 Bonjour je m'appelle Jean-Rayan, Développeur web junior, reconverti depuis 2022, j'ai débuté à 
                 <span> l'ETNA</span> en Bac+2 où j'ai acquis des bases solides en développement. En 2023, j'ai poursuivi 
-                ma formation d'intégrateur web chez<span> OpenClassrooms </span> ce qui m'a permis de renforcer mes compétences,de m’auto-former sur de nouvelles technologies et de m’adapter rapidement à différents outils.
-                À partir de Mars 2025, je reprendrai mes études en alternance en <span>Bachelor 3 "Concepteur de projets SI"</span> à l'ETNA.
+                ma formation d'intégrateur web chez<span> OpenClassrooms </span> ce qui m'a permis de renforcer mes compétences et de m’auto-former sur de nouvelles technologies et de m’adapter rapidement à différents outils.
+                À partir de Mars 2025, je reprendrai mes études en alternance en <span>Bachelor 3 "Concepteur développeur d'application"</span> à l'ETNA.
                 </h3>
             </div>
         </section>
